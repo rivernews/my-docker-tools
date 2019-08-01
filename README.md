2f81ff124fd3f81e593189b8e4c187f61bd2034f
Shaung Cheng <shcheng@paloaltonetworks.com>
2019-08-01 15:30:07 -0700

    add network create

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # my-docker-tools
 
 Usage: either run the script, or cherry pick the commands in the script.
+
+## Useful Docker Commands
+
+- Execute in docker container: `docker exec -it <container name> /bin/bash`
+- Create a user-defined bridge network: `docker network create --driver=bridge my-bridge-network`
--- cleanup.sh
+++ cleanup.sh
@@ -3,21 +3,55 @@
 
 # docker container prune  # removing all stopped containers
 
-docker images -f dangling=true # list all dangling images
+echo INFO: list all containers
+docker container ls
 
 # docker image prune --all # removing dangling AND ununsed images
 
+if [[ $1 == '' ]]
+then
+    PROJECTS_REGEX="(vsc-hubble|vsc-portal)"
+    echo INFO: give an argument like ${PROJECTS_REGEX}
+    return
+else
+    PROJECTS_REGEX=${1}
+fi
 
-PROJECTS_REGEX="(vsc-hubble|vsc-portal)"
+CONTAINERS=$(docker container ps --all | grep -E ${PROJECTS_REGEX} | awk '{print $1}') # -E stands for more extensive regular expression
+IMAGES=$(docker images --all | grep -E ${PROJECTS_REGEX} | awk '{print $3}') # -E stands for more extensive regular expression
+
+if [[ ${CONTAINERS} == '' ]]
+then
+    echo INFO: no container to remove
+else
+    # remove images of projects
+    docker stop ${CONTAINERS}
+    docker container rm ${CONTAINERS}
+fi
+
+if [[ ${IMAGES} == '' ]]
+then
+    echo INFO: no image to remove
+else
+    # remove images of projects
+    docker rmi ${IMAGES}
+fi
 
 
-# remove images of projects
-docker container rm \
-$(docker container ps --all | grep -E ${PROJECTS_REGEX} | awk '{print $1}') # -E stands for more extensive regular expression
 
-# remove images of projects
-docker rmi \
-$(docker images --all | grep -E ${PROJECTS_REGEX} | awk '{print $3}') # -E stands for more extensive regular expression
+# clean up
+echo INFO: list all dangling images
+docker images -f dangling=true
+
+echo INFO: cleaning dangling images whose repository is none...
+DANGLING_IMAGES=$(docker images -f dangling=true | grep -E "<none>" | awk '{print $3}')
+DANGLING_IMAGES=$(docker images -f dangling=true -q)
+if [[ DANGLING_IMAGES == '' ]]
+then
+    echo INFO: no dangling image to clean up.
+else
+    docker rmi ${DANGLING_IMAGES}
+fi
 
 
 # reference
